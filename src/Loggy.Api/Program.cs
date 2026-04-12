d8c6abe26f1247b3213cdab74cd373bb8b13cf48
jakob andersson <jakob.aah@gmail.com>
2026-04-12 23:44:58 +0200

    cleanup: Incorrectly added, logging is low prio atm

--- src/Loggy.Api/Program.cs
+++ src/Loggy.Api/Program.cs
@@ -3,7 +3,6 @@ using Loggy.Core.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext(builder.Configuration.GetConnectionString("LoggyDb") ?? "Data Source=loggy.db");
-builder.Services.AddLogging();
 
 var app = builder.Build();
 app.Services.EnsureDbCreated();
