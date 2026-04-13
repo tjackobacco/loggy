9348bb1b36c1ca1b93b0548c8c6de3b74e738dd2
jakob andersson <jakob.aah@gmail.com>
2026-04-13 12:22:20 +0200

    feat: Add exceptionhandler-fallback Goal to always display something to consumer todo; Make the parametred validations follow this style

---
+++ src/Loggy.Api/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Loggy.Api.ErrorHandling;
+
+/// <summary>Error handling catch-all & return consistently formatted error JSON to consumer</summary>
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellation)
+    {
+        var (status, message) = exception switch
+        {
+            BadHttpRequestException => (StatusCodes.Status400BadRequest, "Bad request"),
+            KeyNotFoundException => (StatusCodes.Status404NotFound, "Not found"),
+            _ => (StatusCodes.Status500InternalServerError, "An unexpected error occurred")
+        };
+
+        logger.LogError("Error {Message}", exception.Message);
+
+        await httpContext.Response.WriteAsJsonAsync(new { message, status });
+
+        return true;
+    }
+}
+
--- src/Loggy.Api/Program.cs
+++ src/Loggy.Api/Program.cs
@@ -1,11 +1,14 @@
 using Loggy.Api.Endpoints.Events;
+using Loggy.Api.ErrorHandling;
 using Loggy.Core.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext(builder.Configuration.GetConnectionString("LoggyDb") ?? "Data Source=loggy.db");
 builder.Services.AddValidation();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 var app = builder.Build();
+app.UseExceptionHandler(_ => { }); // Smother the default with a pillow and make it bubble to GlobalExceptionHandler
 app.Services.EnsureDbCreated();
 app.MapEventEndpoints();
 
